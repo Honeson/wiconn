0e1a5c8ec0b595f1a4c87bd3e443a0cafcb5125e
Nwovu Sunday <honesty1ofa1i@gmail.com>
2020-08-21 13:39:04 +0100

    lastly

--- wiconn_project/settings.py
+++ wiconn_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vb6o4sskhvdf4uz+77$nytc@2avqku@8ql_@i#^#p20r=pfv^2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://wiconn.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
